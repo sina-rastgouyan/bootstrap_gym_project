8aa9e5440e75075adb9d8f746ce296233b2a87d3
sina rastgouyan <sina.rastgouyan@gmail.com>
2022-01-10 15:11:23 +0330

    Gallery Section Has Been Designed In This Step

--- scripts/main.js
+++ scripts/main.js
@@ -26,3 +26,15 @@ $(window).scroll(function () {
         $('.award-img').removeClass('fromLeft')
     }
 })
+
+$('.gallery-list-item').click(function () {
+    let value = $(this).attr('data-filter');
+    $(this).addClass('active-item').siblings().removeClass('active-item')
+
+    if (value === 'all') {
+        $('.filter').show(300)
+    } else {
+        $('.filter').not('.' + value).hide(300);
+        $('.filter').filter('.' + value).show(300);
+    }
+})
